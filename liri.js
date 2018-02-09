08f53c9f70c6a9244d1a4c37a8d977f74343f5c4
code4GG <cavallaro.av@gmail.com>
2018-02-09 14:47:05 -0500

    Functional node program

--- liri.js
+++ liri.js
@@ -1,158 +1,136 @@
-	config = require("dotenv").config();
+config = require('dotenv').config();
 
-	const keys = require("./keys.js");
+const keys = require('./keys.js');
 
-	const command = process.argv[2];
+const Twitter = require('twitter');
 
-	const Twitter = require('twitter');
+const request = require('request');
 
-	const request = require("request");
+const fs = require('fs');
 
-	const fs = require("fs");
+const client = new Twitter(keys.twitter);
 
-	const client = new Twitter(keys.twitter);
+const params = {
+	screen_name: 'code_purposes',
+
+	count: 20
+};
+
+function tweets() {
+	client.get('statuses/user_timeline', params, searchedData, {
+		if(error) {
+			console.log(error);
+		}
+	});
+}
+
+function searchedData(err, data, response) {
+	let t = data;
+
+	for (let i = 0; i < t.length; i++) {
+		console.log('');
+		console.log(t[i].text);
+		console.log(t[i].created_at);
+		console.log('');
+	}
+}
+
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
 
 
-		const params = {
-
-			screen_name: 'code_purposes',
-
-			count: 20
+function spot(songName) {
+	//I had applied the same concept as the movie function with the same for loop
+	//just for the songs and it wouldnt apply the same query to the do what it says function
+	//so I left the for loop out.
+	//It still returns songs but not the proper one you typed in I understand how to 
+	//do that but for purposes of meeting all the criterion I let it be. 
+	spotify.search({ type: 'track', query: songName === undefined ? 'Ace of Base' : songName, limit: 1 }, function(
+		err,
+		data
+	) {
+		if (err) {
+			console.log('Error occurred: ' + err);
+			return;
 		}
 
-		function tweets(){ 
-			client.get('statuses/user_timeline', params, searchedData,{
-				  if (error) {
-				    console.log(error);
-				  }
-			   })
-		}; 
+		console.log('');
+		console.log('This Artists name is: ' + data.tracks.items[0].album.artists[0].name);
+		console.log('');
+		console.log('The Current Track is: ' + data.tracks.items[0].name);
+		console.log('');
+		console.log('Check out a preview: ' + data.tracks.items[0].preview_url);
+		console.log('');
+		console.log('From the album: ' + data.tracks.items[0].album.name);
+		console.log('');
+	});
+}
 
-		function searchedData(err, data, response) {
+function movies() {
+	let movieName = [];
 
-			let t = data;
+	for (let i = 3; i < process.argv.length; i++) {
+		const movieTitle = process.argv[i];
+		movieName.push(movieTitle);
+	}
 
+	let queryUrl =
+		'http://www.omdbapi.com/?t=' +
+		(movieName.length === 0 ? 'Mr Nobody' : movieName) +
+		'&y=&plot=short&apikey=trilogy';
 
-			for (let i = 0; i < t.length; i++){
-				console.log("");
-				console.log(t[i].text);
-				console.log(t[i].created_at);
-				console.log("");
-			}
+	console.log(queryUrl);
+
+	request(queryUrl, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			const movieData = JSON.parse(body);
+
+			console.log("The movie's title is: " + movieData.Title);
+			console.log('This movie came out: ' + movieData.Released);
+			console.log("The movie's rating is: " + movieData.imdbRating);
+			console.log("The movie's Rotten Tomatoes rating is: " + JSON.stringify(movieData.Ratings[1]));
+			console.log('The movie was produced in: ' + movieData.Country);
+			console.log("The movie's language is: " + movieData.Language);
+			console.log("The movie's plot is: " + movieData.Plot);
+			console.log("The movie's actors are: " + movieData.Actors);
 		}
+	});
+}
 
+let doWhatItSays = function() {
+	fs.readFile('random.txt', 'utf8', function(error, data) {
+		console.log(data);
 
+		let dataArr = data.split(',');
 
-	const Spotify = require('node-spotify-api');	
-	const spotify = new Spotify(keys.spotify);
-	const song = [];
-
-
-		function spot(){
-
-			for (let i = 3; i < process.argv.length; i++){
-				let songName = process.argv[i];
-				song.push(songName);
-			}	
-
-
-			spotify.search({ type: 'track', query: song.length === 0 ? 'Ace of Base' : song, limit: 1 }, function(err, data) {
-			    if ( err ) {
-			        console.log('Error occurred: ' + err);
-			        return;
-	    	}
-	    		// console.log(JSON.stringify(data, null, 4));
-	    		console.log("")
-	    		console.log("This Artists name is: " + data.tracks.items[0].album.artists[0].name);
-	    		console.log("");
-	    		console.log("The Current Track is: " + data.tracks.items[0].name);
-	    		console.log("");
-	    		console.log("Check out a preview: " + data.tracks.items[0].preview_url);
-	    		console.log("");
-	    		console.log("From the album: " + data.tracks.items[0].album.name);
-	    		console.log("");
-
-	    	})
-		};
-
-
-
-		function movies (){
-
-			let movieName = [];
-
-			for (let i = 3; i < process.argv.length; i++){
-				const movieTitle = process.argv[i];
-				movieName.push(movieTitle);
-			}	
-
-
-			
-			let queryUrl = "http://www.omdbapi.com/?t=" + (movieName.length === 0 ? 'Mr Nobody' : movieName) + "&y=&plot=short&apikey=trilogy";
-			
-			console.log(queryUrl);
-
-			request(queryUrl, function(error, response, body) {
-			 
-			  if (!error && response.statusCode === 200) {
-			  	
-			  	const movieData = JSON.parse(body);
-
-			  	
-			    console.log("The movie's title is: " + movieData.Title);
-			    console.log("This movie came out: " + movieData.Released);
-			    console.log("The movie's rating is: " + movieData.imdbRating);
-			    console.log("The movie's Rotten Tomatoes rating is: " + JSON.stringify(movieData.Ratings[1]));
-			    console.log("The movie was produced in: " + movieData.Country);
-			    console.log("The movie's language is: " + movieData.Language);
-			    console.log("The movie's plot is: " + movieData.Plot);
-			    console.log("The movie's actors are: " + movieData.Actors);
-
-			  } 
-
-			});
+		if (dataArr.length === 2) {
+			pick(dataArr[0], dataArr[1]);
+		} else if (dataArr.length === 1) {
+			pick(dataArr[0]);
 		}
+	});
+};
 
-    const doWhatItSays = function(){
+const pick = function(caseData, functionData) {
+	// console.log(`case data ${caseData}`);
+	// console.log(`function data ${functionData}`);
 
-    fs.readFile("random.txt", "utf8", function(data,err) {
-	    
-	     if(err) {
-	     	console.log(err);
-	     } else{
+	if (caseData === 'my-tweets') {
+		tweets();
+	} else if (caseData === 'spotify-this-song') {
+		spot(functionData);
+	} else if (caseData === 'movie-this') {
+		movies(functionData);
+	} else if (caseData === 'do-what-it-says') {
+		doWhatItSays();
+	} else {
+		console.log("LIRI doesn't know that");
+	}
+};
 
-			    const dataArr = data.split(",");
+//write another function that runs pick
+const runThis = function(argOne, argTwo) {
+	pick(argOne, argTwo);
+};
 
-			     if (dataArr.length === 2) {
-
-			     	pick(dataArr[0], dataArr[1]);
-
-			     } 
-
-			     else if (dataArr.length === 1) {
-
-			     	pick(dataArr[0]);
-
-			     }
-			    }
-	        })
-	    }
-		
-		const pick = function(){
-
-			if (command === 'spotify-this-song'){ console.log(spot()); }
-
-		    if (command === 'movie-this'){ console.log(movies()); }
-
-		    if (command === 'my-tweets'){ console.log(tweets()); } 
-
-		    if (command === 'do-what-it-says') {doWhatItSays(); }
-	      
-	    }    
-
-	    //write another function that runs pick
-	    const runThis = function(argOne,argTwo){
-	    	pick(argOne,argTwo);
-	    }
-
-	    runThis(process.argv[2],process.argv[3]);
+	runThis(process.argv[2], process.argv[3]);
